d4777372fff58c86423409ef26b83b5c37686cb7
MiLkTeA <43979198+18Grace18@users.noreply.github.com>
2019-11-07 19:11:28 +0700

    Create Bina8.c

---
+++ Bina8.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() {
+int n,r,i,j,c,o;
+i=0;
+j=1;
+c=1;
+scanf("%d,&n);
+int a[];
+while(n>0){
+r=n%10;
+o+=r*j;
+if(c/3==0) {
+a[i]=o;
+j=1;
+c=1;
+o=0;
+i++;
+}
+else{
+j*=2;
+c++
+}
+n/=10;
+}
+if(c!=1){
+a[i]=o;
+}
+for(i=i;i>-1;i--){
+printf("%d",a[i]);
+}
+return 0;
+}
